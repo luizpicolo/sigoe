c253b22d91545a6ae71e4502c180246935bd2f0a
Luiz Picolo <luizpicolo@gmail.com>
2017-05-07 23:25:18 -0400

    Criar spec features para entidade Keypass
    - Aqui vai...

---
+++ spec/features/keypass/search.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+context "Find users by your search scope" do
+  before(:each) do
+    @sector = FactoryGirl.create(:sector)
+    @users = FactoryGirl.create_list(:user, 2, sector: @sector)
+  end
+
+  feature 'search user' do
+    ['name', 'email', 'siape', 'course'].each do |attr|
+      scenario "find with attribute #{attr}" do
+        sign_in @users.first
+        visit users_path
+        fill_in 'search', with: @users.first.read_attribute(attr)
+        click_button 'Buscar'
+        expect(page).to have_content(@users.first.name)
+      end
+    end
+  end
+end
