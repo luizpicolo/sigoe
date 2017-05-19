ded0e33526f747ab11bb9127763c041666058a3c
Luiz Picolo <luizpicolo@gmail.com>
2017-05-19 09:21:15 -0400

    Incrementado cobertura de testes

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,10 +1,22 @@
 require "rails_helper"
 
-describe ApplicationHelper do
+RSpec.describe ApplicationHelper, type: :helper do
   describe "#show_search_error_message" do
     it "returns error message if entity not exist" do
       params[:search] = "message"
       expect(helper.show_search_error_message(nil, params)).to eql("Sua busca por <b>#{params[:search]}</b> não obteve resutados")
+    end
+  end
+
+  describe "#convert_to_entity" do
+    it "convert Activerecord::relation to object" do
+      expect(helper.convert_to_entity(User.all)).to eql(User)
+    end
+  end
+
+  describe "#attr_order" do
+    it "return ordenation attributes" do
+      expect(helper.attr_order(User.all)).to eql(User.ordenation_attributes)
     end
   end
 end
