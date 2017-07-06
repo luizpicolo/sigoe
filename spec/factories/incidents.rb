28188c41b77e8479525625343132efc77f672919
Luiz Picolo <luizpicolo@gmail.com>
2017-07-06 08:57:59 -0400

    Adicionado atributo assistente de aluno para ocorrências

--- spec/factories/incidents.rb
+++ spec/factories/incidents.rb
@@ -5,18 +5,22 @@
 #  id            :integer          not null, primary key
 #  student_id    :integer
 #  user_id       :integer
-#  local         :integer
+#  institution   :integer
 #  description   :text
 #  date_incident :date
 #  soluction     :text
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
+#  course_id     :integer
+#  time_incident :time
+#  assistant_id  :integer
 #
 
 FactoryGirl.define do
   factory :incident do
     student factory: :student
     user factory: :user
+    assistant factory: :user
     course factory: :course
     institution { 'ifms' }
     description { Faker::Lorem.words(255) }
