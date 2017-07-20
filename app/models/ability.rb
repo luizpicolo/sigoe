afd146d13027bd1b280c1d264b72b1576bf9a7f4
Luiz Picolo <luizpicolo@gmail.com>
2017-07-20 09:48:09 -0400

    Adicionado comentários para setores

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,28 +8,34 @@ class Ability
   def initialize(user)
     user ||= User.new
 
+    # Assistente de alunos
     if user.it_is_part_of_the_sector?('assal')
       can :manage, Incident
     end
 
+    # Audio visual
     if user.it_is_part_of_the_sector?('audi')
       can :manage, Student
     end
 
+    # Diretoria de ensino
     if user.it_is_part_of_the_sector?('diren')
       can :manage, Student
       can :manage, Course
       can :manage, Incident
     end
 
+    # Professores
     if user.it_is_part_of_the_sector?('prof')
       can :read, Student
     end
 
+    # admins
     if user.it_is_part_of_the_sector?('serti')
       can :manage, :all
     end
 
+    # Tecnicos de Lab em Informatica
     if user.it_is_part_of_the_sector?('teclabinf')
       can :read, Keypass
     end
