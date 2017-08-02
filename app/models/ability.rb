fceafcba0bc265b387e0fc4e44c24cc4d1808ded
Luiz Picolo <luizpicolo@gmail.com>
2017-08-02 15:39:56 -0400

    Adicionar habilidades para os usuários
    - Adicionada regra para retornar para a página solicitante/root todos os
    acessos negados;
    - Modificada habilidades dos usuários para refletir na regra de negócio
    (regras de acesso);
    - Modificada views para ocultar ou mostrar ações de acordo com o
    usuário (esta parte é apenas visual, pois o cancancan cuida disso de
    forma transparente);
    - Corrigida specs.
    -

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,19 +10,19 @@ class Ability
 
     # Assistente de alunos
     if user.it_is_part_of_the_sector?('assal')
-      can :manage, Incident
+      can [:create, :update, :read], Incident
     end
 
     # Audio visual
     if user.it_is_part_of_the_sector?('audi')
-      can :manage, Student
+      can [:read, :update], Student
     end
 
     # Diretoria de ensino
     if user.it_is_part_of_the_sector?('diren')
-      can :manage, Student
-      can :manage, Course
+      can [:update, :read], Student
       can :manage, Incident
+      cannot :destroy, Incident
     end
 
     # Professores
@@ -36,7 +36,7 @@ class Ability
     end
 
     # Tecnicos de Lab em Informatica
-    if user.it_is_part_of_the_sector?('teclabinf')
+    if user.it_is_part_of_the_sector?('teclabinfo')
       can :read, Keypass
     end
   end
