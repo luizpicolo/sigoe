f0e8e7c7769a1f6035e1d560f374c8cea59b608c
Luiz Picolo <luizpicolo@gmail.com>
2017-12-20 14:51:29 -0300

    Adicionado métodos para ordenação de atributos

--- app/controllers/keypasses_controller.rb
+++ app/controllers/keypasses_controller.rb
@@ -10,7 +10,9 @@ class KeypassesController < ApplicationController
     add_breadcrumb "Serti", sector_actions_path('serti')
     add_breadcrumb "Senhas"
 
-    @keypasses = Keypass.search(params[:search]).order('id asc').page(params[:page])
+    @keypasses = Keypass.order("#{set_order}": :desc)
+                        .search(params[:search])
+                        .page(params[:page]).per(set_amount_return)
   end
 
   def new
