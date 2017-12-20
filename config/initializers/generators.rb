b4ff4459963320816a9d7fdfd1027acb358e8280
Luiz Picolo <luizpicolo@gmail.com>
2017-12-20 14:31:41 -0300

    Fixed: Modificado gerador de spec para FactoryBot

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -6,7 +6,7 @@ Rails.env.on(:any) do
   # don't generate RSpec tests for views and helpers
   config.generators do |g|
     g.test_framework :rspec, fixture: true
-    g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    g.fixture_replacement :factory_bot, dir: 'spec/factories'
     g.view_specs false
     g.helper_specs false
   end
