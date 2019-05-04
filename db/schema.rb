f9f53d8962549b21ba43560a3f4999d5d214b938
Luiz Picolo <luizpicolo@gmail.com>
2019-05-04 00:34:47 -0400

    Atualizados

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2019_04_27_014051) do
     t.integer "type_student"
     t.integer "sanction"
     t.integer "school_group_id"
-    t.integer "type_incident"
     t.bigint "type_incident_id"
     t.index ["course_id"], name: "index_incidents_on_course_id"
     t.index ["date_incident"], name: "index_incidents_on_date_incident"
@@ -225,24 +224,6 @@ ActiveRecord::Schema.define(version: 2019_04_27_014051) do
     t.index ["name"], name: "index_students_on_name"
   end
 
-  create_table "tickets", id: :serial, force: :cascade do |t|
-    t.string "from"
-    t.string "to"
-    t.string "subject"
-    t.integer "priority", default: 0
-    t.text "description"
-    t.integer "status", default: 0
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "local"
-    t.integer "answer"
-    t.index ["from"], name: "index_tickets_on_from"
-    t.index ["status"], name: "index_tickets_on_status"
-    t.index ["to"], name: "index_tickets_on_to"
-    t.index ["user_id"], name: "index_tickets_on_user_id"
-  end
-
   create_table "type_incidents", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -287,7 +268,6 @@ ActiveRecord::Schema.define(version: 2019_04_27_014051) do
   add_foreign_key "patients", "students"
   add_foreign_key "permissions", "users"
   add_foreign_key "students", "courses"
-  add_foreign_key "tickets", "users"
   add_foreign_key "users", "courses"
   add_foreign_key "users", "sectors"
 end
