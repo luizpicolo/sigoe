aa193fdc58a4ff8f555dbc84bba5d9d0ed9730a1
Luiz Picolo <luizpicolo@gmail.com>
2019-05-04 00:34:35 -0400

    Criado Seed para primeiro usuário

---
+++ db/data/20190503183710_add_first_user.rb
@@ -0,0 +1,20 @@
+class AddFirstUser < SeedMigration::Migration
+  def up
+    Sector.create({
+      name: 'Diretoria de Ensino',
+      initial: 'DIREN',
+      icon: 'ion-university'
+    })
+
+    User.create({
+      name: 'admin',
+      siape: '12345678',
+      sector_id: Sector.first.id,
+      username: 'admin',
+      email: 'admin@admin.com',
+      password: '12345678',
+      password_confirmation: '12345678',
+      admin: true
+    })
+  end
+end
