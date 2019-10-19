a5d79b0b6bcf588b2061ac4c81839e55cc395390
Luiz Picolo <luizpicolo@gmail.com>
2019-10-18 21:28:51 -0400

    Removido ClipBoardJS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery-mask-plugin/dist/jquery.mask
 //= require jquery.turbolinks
@@ -21,25 +9,9 @@
 //= require alert_message
 //= require highcharts
 //= require chartkick
-//= require clipboard
 //= require turbolinks
 
 document.addEventListener('turbolinks:load', function() {
-  //$('.select2').select2();
-
-  // Clipboard JS
-  var clipboard = new Clipboard('.clipboard');
-
-  clipboard.on('success', function(e) {
-    alert('Senha copiada para sua área de tranferência');
-    e.clearSelection();
-  });
-
-  clipboard.on('error', function(e) {
-      console.error('Action:', e.action);
-      console.error('Trigger:', e.trigger);
-  });
-
   // Select 2
   $(".select2").select2();
 
