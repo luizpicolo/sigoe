80b50e13903f09dd4dd3fdc52b322c472b1c629a
Luiz Picolo <luizpicolo@gmail.com>
2019-10-31 21:17:16 -0400

    Adicionado configuração adicional para assets no Heroku

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Sigoe
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Heroku 
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
