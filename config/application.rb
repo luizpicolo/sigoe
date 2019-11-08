e0a574ff5d7cd345e25867d70fc3967d55177cea
Luiz Picolo <luizpicolo@gmail.com>
2019-11-07 23:47:18 -0400

    testes

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Sigoe
 
     # Heroku 
     config.serve_static_assets = true
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
