1bda2bf0ab850b8ad22f3b78bdf0410a99b9893b
Luiz Picolo <luizpicolo@gmail.com>
2021-01-29 09:52:33 -0400

    Atualizada busca para email do admin A busca agora retorna o email, apenas do administrador local

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
   add_breadcrumb 'Home', :root_path
 
   def index
-    @email_adm = User.find_by_admin(true)
+    @email_adm = User.where(admin: true).where.not(email: 'luiz.picolo@ifms.edu.br').first
   end
 end
