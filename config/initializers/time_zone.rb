df4298bbb1a775c2c66ae3fabd598b513c5b3f2f
Luiz Picolo <luizpicolo@gmail.com>
2022-03-14 17:36:54 -0400

    Alterado configuração default timezone activerecord

--- config/initializers/time_zone.rb
+++ config/initializers/time_zone.rb
@@ -4,4 +4,5 @@ Rails.env.on(:any) do
   # Configuration default for time zone
   config.time_zone = 'La Paz'
   config.i18n.default_locale = :'pt-BR'
+  config.active_record.default_timezone = :local
 end
