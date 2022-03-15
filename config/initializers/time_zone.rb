ccf6bfc4527f695ed9ed30fe21ed7436350f8351
Luiz Picolo <luizpicolo@gmail.com>
2022-03-14 23:15:21 -0400

    Alterado config.time_zone

--- config/initializers/time_zone.rb
+++ config/initializers/time_zone.rb
@@ -2,7 +2,7 @@
 
 Rails.env.on(:any) do
   # Configuration default for time zone
-  config.time_zone = 'La Paz'
+  config.time_zone = 'Atlantic Time (Canada)'
   config.i18n.default_locale = :'pt-BR'
   config.active_record.default_timezone = :utc
 end
