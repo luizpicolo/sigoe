c25e40992ad5e36b7c93b58fc0c47c6f79803b74
Luiz Picolo <luizpicolo@gmail.com>
2022-03-20 21:05:55 -0400

    Modificado timezone para local

--- config/initializers/time_zone.rb
+++ config/initializers/time_zone.rb
@@ -4,5 +4,5 @@ Rails.env.on(:any) do
   # Configuration default for time zone
   config.time_zone = 'Atlantic Time (Canada)'
   config.i18n.default_locale = :'pt-BR'
-  config.active_record.default_timezone = :utc
+  config.active_record.default_timezone = :local
 end
