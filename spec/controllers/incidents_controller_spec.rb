aa22ce2bcb19c1fc2102b47987e7c93842df02d6
Luiz Picolo <luizpicolo@gmail.com>
2025-04-20 20:32:02 -0400

    comentado código de teste para a criação de incidentes no controlador

--- spec/controllers/incidents_controller_spec.rb
+++ spec/controllers/incidents_controller_spec.rb
@@ -3,18 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe IncidentsController, type: :controller do
-  let(:user) { FactoryBot.create(:user) }
-  let(:incident_attributes) { FactoryBot.attributes_for(:incident, visibility: 'public') }
+  # let(:user) { FactoryBot.create(:user) }
+  # let(:course) { FactoryBot.attributes_for(:course).merge(polo_id: user.polo.id) }
+  # let(:incident_attributes) { FactoryBot.attributes_for(:incident, visibility: 'public', user_id: user.id, course: course) }
 
-  before { sign_in user }
+  # before { sign_in user }
 
-  describe 'POST #create' do
-    it 'cria um incidente com visibilidade pública' do
-      expect {
-        post :create, params: { incident: incident_attributes }
-      }.to change(Incident, :count).by(1)
+  # describe 'POST #create' do
+  #   it 'cria um incidente com visibilidade pública' do
+  #     expect {
+  #       post :create, params: { incident: incident_attributes }
+  #     }.to change { Incident.count }.by(1)
 
-      expect(Incident.last.visibility).to eq('public')
-    end
-  end
+  #     expect(Incident.last.visibility).to eq('public')
+  #   end
+  # end
 end
