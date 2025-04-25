188a2c0438a4485e131681715768acc54bc48dfb
Luiz Picolo <luizpicolo@gmail.com>
2025-04-24 22:37:04 -0400

    adicionada lógica para habilitar/desabilitar permissões com base na seleção da entidade; atualizado o select para incluir tradução

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,25 @@ Turbolinks.start();
 ActiveStorage.start();
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Campo de seleção
+  const entitySelect = document.getElementById("entity-select");
+
+  entitySelect.addEventListener("change", function() {
+    const permissions = ['can_extras', 'can_read_restricted', 'can_export_to_academic_system']
+
+    permissions.forEach((permission) => {
+      const input = document.querySelector(`input[name="permission[${permission}]"][type="checkbox"]`);
+      if (this.value != "Incident"){
+        input.disabled = true;
+        input.checked = false;
+      } else {
+        input.disabled = false;
+        input.checked = false;
+      }
+    })
+  });
+  
+  // Alertas
   let alertMessage = document.getElementById('alert');
   
   alertMessage.classList.add('alert-message--visible');
